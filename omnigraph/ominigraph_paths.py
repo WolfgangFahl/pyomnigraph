1d19c5f18e3e40aa837f2fb237094272704a6158
Wolfgang Fahl <wf@bitplan.com>
2025-06-03 10:20:40 +0200

    tries fixing CI with pseudo-home

--- omnigraph/ominigraph_paths.py
+++ omnigraph/ominigraph_paths.py
@@ -21,7 +21,7 @@ class OmnigraphPaths:
         """
         self.home_dir = home_dir if home_dir else Path.home()
         self.omnigraph_dir = self.home_dir / ".omnigraph"
-        self.omnigraph_dir.mkdir(exist_ok=True)
+        self.omnigraph_dir.mkdir(parents=True, exist_ok=True)
         self.dumps_dir = self.omnigraph_dir / "rdf_dumps"
         self.dumps_dir.mkdir(exist_ok=True)
         self.examples_dir = (Path(__file__).parent / "resources" / "examples").resolve()
